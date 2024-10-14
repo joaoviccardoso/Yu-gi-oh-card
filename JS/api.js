74e54d09ade4bd0149bf00404f2c7f6bd7458436
joao victor da silva cardoso <pealq63@gmail.com>
2024-10-14 16:02:33 -0300

    contruido o a estrutura do html do aside esquerdo

--- JS/api.js
+++ JS/api.js
@@ -1,7 +1,12 @@
+const main = document.querySelector(".main-centro");
+
 async function pegarDadosDaApi() {
-    const resposta = await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php");
+    const resposta = await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark Magician");
     const dadosApi = await resposta.json()
     console.log(dadosApi)
+    main.innerHTML = `
+    <img src="${dadosApi.data[0].card_images[0].image_url_small}" alt="seila" width="200">
+    `
 }
 
 pegarDadosDaApi()
