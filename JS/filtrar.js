6b44faf7cda28039fa4170c926cbf95eaa98212f
joao victor da silva cardoso <pealq63@gmail.com>
2024-10-29 23:26:43 -0300

    feat: adicionar filtro assíncrono para cards com base na categoria selecionada
    - Alterado `onclick` do botão `btnFiltrar` para uma função assíncrona, permitindo o uso de `await`.
    - Adicionada chamada a `api.pegarDadosDaApi()` para obter dados dos cards e exibi-los no console para validação.
    - Implementado filtro para selecionar cards cuja propriedade `type` contenha o valor da categoria (`CardCategory`) selecionada pelo usuário.
    - Exibição dos cards filtrados no console para verificação do filtro.
    
    Essas mudanças permitem obter dados da API e aplicar um filtro de categoria de forma assíncrona.

--- JS/filtrar.js
+++ JS/filtrar.js
@@ -1,10 +1,17 @@
 const btnFiltrar = document.getElementById("filtrar");
 
-btnFiltrar.onclick = () =>{
+btnFiltrar.onclick = async () =>{
     const CardCategory = document.querySelector(".category").value;
     const level = document.querySelector(".level").value;
     const attribute = document.querySelector(".attribute").value;
     const type = document.querySelector(".type").value;
 
-    console.log(CardCategory, level, attribute, type)
+    const dadosDaApi = await api.pegarDadosDaApi()
+    console.log(dadosDaApi)
+
+    const cardFiltrados = await dadosDaApi.data.filter(card =>{
+        return card.type.includes(CardCategory)
+    })
+
+    console.log(cardFiltrados)
 }
