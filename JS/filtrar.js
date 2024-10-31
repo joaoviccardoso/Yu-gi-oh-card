a0b31393955d1111dcff53d48ee5d556a590442b
joao victor da silva cardoso <pealq63@gmail.com>
2024-10-31 00:58:33 -0300

    Atualiza a função de filtragem de cartas para incluir filtros de nível, atributo e tipo

--- JS/filtrar.js
+++ JS/filtrar.js
@@ -9,9 +9,25 @@ btnFiltrar.onclick = async () =>{
     const dadosDaApi = await api.pegarDadosDaApi()
     console.log(dadosDaApi)
 
-    const cardFiltrados = await dadosDaApi.data.filter(card =>{
-        return card.type.includes(CardCategory)
+    const cardFiltradosCategory = await dadosDaApi.data.filter(card =>{
+        const matchCategory = CardCategory ? card.type.includes(CardCategory) : true; 
+        return matchCategory
     })
 
-    console.log(cardFiltrados)
+    const cardFiltradosLevel = await dadosDaApi.data.filter(card => {
+        const matchLevel = level ? card.level == Number(level) : true;
+        return matchLevel
+    })
+
+    const cardFiltradosAttribute = await dadosDaApi.data.filter(card =>{
+        const matchAttribute = attribute ? card.attribute == attribute : true;
+        return matchAttribute
+    })
+
+    const cardFiltradosType = await dadosDaApi.data.filter(card => {
+        const matchType = type ? (card.typeline && Array.isArray(card.typeline) && card.typeline[0].includes(type)) : true;
+        return matchType
+    })
+
+    console.log(cardFiltradosCategory, cardFiltradosAttribute, cardFiltradosLevel, cardFiltradosType)
 }
