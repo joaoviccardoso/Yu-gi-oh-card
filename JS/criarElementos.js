951302a805028106c47f1847442bd34f123d6c0a
seu nome de usuário <pealq63@gmail.com>
2024-11-13 16:05:59 -0300

    feat: adicionar melhorias de UI e funcionalidade do botão de fechar
    - Adicionado um botão de fechar com ícone na seção de detalhes da carta para melhor interação do usuário.
    - Incluído um botão para adicionar uma carta ao deck, aumentando a funcionalidade.
    - Estruturados contêineres para organizar botões, imagens das cartas e detalhes das cartas.
    - Melhorada a legibilidade e manutenção do código com comentários estruturados e seções claras.
    - Sem alterações que quebrem funcionalidades; os novos recursos melhoram a experiência do usuário sem modificar o comportamento existente.

--- JS/criarElementos.js
+++ JS/criarElementos.js
@@ -4,12 +4,36 @@ const asideEsquerda = document.querySelector(".container-aside-esquerdo")
 async function criarElementoEsquesta(dadosApi){
     asideEsquerda.innerHTML = "";
 
+    //Cria o container principal das card
     const divContainerPrincipal = document.createElement("div");
     divContainerPrincipal.classList.add("container-info-esquerda");
 
     const divContainerInfoImg = document.createElement("div");
     divContainerInfoImg.classList.add("container-info-img");
 
+    //Cria o container do btn fechar janela e o btn add
+    const containerBtnFechar = document.createElement("div");
+    containerBtnFechar.classList.add("container-btn-fechar");
+
+    const btnFecharJanela = document.createElement("button");
+    btnFecharJanela.classList.add("btn-fechar-janela");
+
+    const iconeFecharJanela = document.createElement("img");
+    iconeFecharJanela.classList.add("icone-fechar-janela");
+    iconeFecharJanela.alt = "fechar janela"
+    iconeFecharJanela.src = "img/excluir.png"
+
+    const btnAddCardAoDeck = document.createElement("button");
+    btnAddCardAoDeck.classList.add("btn", "btn-primary");
+    btnAddCardAoDeck.id = "btnAdd"
+    btnAddCardAoDeck.textContent = "Add Card"
+
+    //adiciona o btn fechar e btn add no container
+    btnFecharJanela.appendChild(iconeFecharJanela)
+    containerBtnFechar.appendChild(btnFecharJanela);
+    containerBtnFechar.appendChild(btnAddCardAoDeck);
+
+    //criar o container das fotos da card
     const imgInfo = document.createElement("img");
     imgInfo.classList.add("img-info");
     imgInfo.alt = "card" 
@@ -18,10 +42,13 @@ async function criarElementoEsquesta(dadosApi){
     const divContainerInfoCard = document.createElement("div");
     divContainerInfoCard.classList.add("container-informação-carta");
 
+    //Cria o container Ul q vai ser colocado nivel, ATK e DEF
     const ulInfo = document.createElement("ul");
     ulInfo.classList.add("ul-informacao-item");
 
     if(dadosApi.data[0].atk){
+
+    //Cria o container Li das estrelas
     const liNivel = document.createElement("li");
 
     const divInfo = document.createElement("div");
@@ -40,6 +67,7 @@ async function criarElementoEsquesta(dadosApi){
     divInfo.appendChild(imgNivel)
     divInfo.appendChild(pDescricaoNivel)
 
+    //Cria o container do atk
     const liAtk = document.createElement("li");
     const divInfo2 = document.createElement("div");
     divInfo2.classList.add("container_infomacao");
@@ -56,7 +84,8 @@ async function criarElementoEsquesta(dadosApi){
     liAtk.appendChild(divInfo2)
     divInfo2.appendChild(imgAtk)
     divInfo2.appendChild(pDescricaoAtk)
-    
+
+    //Cria o container da Def
     const liDef = document.createElement("li");
     const divInfo3 = document.createElement("div");
     divInfo3.classList.add("container_infomacao");
@@ -74,6 +103,7 @@ async function criarElementoEsquesta(dadosApi){
     divInfo3.appendChild(imgDef)
     divInfo3.appendChild(pDescricaoDef)
 
+    //Add todas as Li na Ul
     ulInfo.appendChild(liNivel)
     ulInfo.appendChild(liAtk)
     ulInfo.appendChild(liDef)
@@ -87,9 +117,11 @@ async function criarElementoEsquesta(dadosApi){
     pDescricaoDaCard.textContent = `${dadosApi.data[0].desc}`;
 
     divDescricao.appendChild(pDescricaoDaCard)
+
     divContainerInfoImg.appendChild(imgInfo)
     divContainerInfoCard.appendChild(ulInfo)
     divContainerInfoCard.appendChild(divDescricao)
+    divContainerPrincipal.appendChild(containerBtnFechar)
     divContainerPrincipal.appendChild(divContainerInfoImg)
     divContainerPrincipal.appendChild(divContainerInfoCard)
     asideEsquerda.appendChild(divContainerPrincipal)
